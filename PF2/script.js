9a2677005d5ae7c8d6e917a534a05ea8724c3c99
gri0005 <s4002908@student.rmit.edu.au>
2024-11-20 18:19:52 +1100

    lot of changes

---
+++ PF2/script.js
@@ -0,0 +1,24 @@
+/*js for button animations*/
+// portfolio button
+function growingPortfolio() {
+  document.getElementById("ringPortfolio").style.scale = "250%";
+  document.getElementById("portfolioImage").style.transform = "rotate(12deg)";
+}
+function shrinkingPortfolio() {
+  document.getElementById("ringPortfolio").style.scale = "100%";
+  document.getElementById("portfolioImage").style.transform = "rotate(-12deg)";
+}
+
+// contact page button
+function growingContact() {
+  document.getElementById("ringContact").style.scale = "250%";
+  document.getElementById("contactImage").style.transform = "rotate(12deg)";
+}
+function shrinkingContact() {
+  document.getElementById("ringContact").style.scale = "100%";
+  document.getElementById("contactImage").style.transform = "rotate(-12deg)";
+}
+
+function goToPF() {
+  document.n;
+}
